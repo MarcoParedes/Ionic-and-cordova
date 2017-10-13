11a6d01d5141c12bb03760640ee4abbafc61d588
Marco Paredes <marcop_93_04@hotmail.com>
2017-10-13 16:10:24 -0300

    Assignment 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { LocalNotifications } from '@ionic-native/local-notifications';
 import { EmailComposer } from '@ionic-native/email-composer';
 import { SocialSharing } from '@ionic-native/social-sharing';
 import { Camera } from '@ionic-native/camera';
+import { Network } from '@ionic-native/network';
+import { CallNumber } from '@ionic-native/call-number';
 import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
 import { PromotionProvider } from '../providers/promotion/promotion';
@@ -77,7 +79,9 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
     FavoriteProvider,
     EmailComposer,
     SocialSharing,
-    Camera
+    Camera,
+    Network,
+    CallNumber
   ]
 })
 export class AppModule {}
